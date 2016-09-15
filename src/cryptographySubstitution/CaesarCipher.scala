960003427ac47227f08e5b07429ef3c649c1cd6e
Zackery-Kiyoshi <zkurimab@trinity.edu>
2016-09-14 20:41:16 -0500

    Finished easy testing

--- src/cryptographySubstitution/CaesarCipher.scala
+++ src/cryptographySubstitution/CaesarCipher.scala
@@ -5,6 +5,10 @@ class CaesarCipher {
   var charToInt = scala.collection.mutable.Map(' '->(-1), 'a'->0,'b'->1,'c'->2,'d'->3,'e'->4,'f'->5,'g'->6,'h'->7,'i'->8,'j'->9,'k'->10,'l'->11,'m'->12,'n'->13,'o'->14,'p'->15,'q'->16,'r'->17,'s'->18,'t'->19,'u'->20,'v'->21,'w'->22,'x'->23,'y'->24,'z'->25)
   var intToChar = List('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
   
+  def encrypt(key:Integer,m:String):String = {
+    return encrypt(key,m,5,"x")
+  }
+  
   def encrypt(key:Integer,m:String, block:Integer, fill:String):String = {
     var ret = ""
     var map = scala.collection.mutable.Map(' ' -> ' ')
@@ -48,12 +52,16 @@ class CaesarCipher {
     return ret
   }
   
-  def decrypt(key:Integer,m:String, blocks:Boolean):String = {
+  def decrypt(key:Integer,m:String):String = {
+    return decrypt(key,m,5)
+  }
+  
+  def decrypt(key:Integer,m:String, blocks:Integer):String = {
     var ret = ""
     var map = scala.collection.mutable.Map('`' -> ' ')
     map -= ('`', ' ')
     var msg = m
-    if(blocks){
+    if(blocks == 0){
       msg = m.replaceAll(" ","")
     }
     //println( "Start: " + intToChar.size)
@@ -84,10 +92,23 @@ class CaesarCipher {
       //map += (m.charAt(x), (b.charAt(x).toInt - base)
     }
     //println( "Done")
+    if(blocks !=0){
+      var i = blocks
+      while(i < ret.length ){
+        val (p1, p2) = ret.splitAt(i)
+        ret = p1 + " " + p2
+        i += blocks+1
+      }
+    }
+    
     return ret
   }
   
   def encrypt(key:String,m:String):String = {
+    return encrypt(key,m,5,"x")
+  }
+  
+  def encrypt(key:String,m:String, block:Integer, fill:String):String = {
     var ret = ""
     var map = scala.collection.mutable.Map(' ' -> ' ')
     map -= (' ', ' ')
@@ -131,6 +152,80 @@ class CaesarCipher {
       //map += (m.charAt(x), (b.charAt(x).toInt - base)
     }
     //println( "Done")
+    var i = block
+    while(i < ret.length ){
+      val (p1, p2) = ret.splitAt(i)
+      ret = p1 + " " + p2
+      i += block+1
+    }
+    if(i > ret.length && fill != ""){
+        for(j <- ret.length until i) ret += fill
+      }
+    
+    return ret
+  }
+  
+  def decrypt(key:String,m:String):String = {
+    return decrypt(key,m,5)
+  }
+  def decrypt(key:String,m:String, blocks:Integer):String = {
+    var ret = ""
+    var map = scala.collection.mutable.Map(' ' -> ' ')
+    map -= (' ', ' ')
+    var msg = m
+    if(blocks == 0){
+      msg = m.replaceAll(" ","")
+    }
+    
+    //println( "Start: " + intToChar.size)
+    var k = key.distinct
+    for( i <- 0 until k.length()){
+      //println( i + ":" + t)
+      var c1:Char = intToChar(i)
+      var c2:Char = k(i)
+      //println( c1 + ":" + c2)
+      map += (c1->c2)
+    }
+    var t = 0
+    for( i <- k.length() until intToChar.size){
+      while(k.contains(intToChar(t))){
+        t+=1
+      }
+      k += intToChar(t)
+      //println( i + ":" + t)
+      var c1:Char = intToChar(i)
+      var c2:Char = intToChar(t)
+      //println( c1 + ":" + c2)
+      map += (c1->c2)
+    }
+    /*
+    for( i <- 0 until intToChar.size){
+      println(intToChar(i) + " => " + map(intToChar(i)))
+    }
+//  */
+    //println( "End")
+    for((x) <- 0 until msg.length()) {
+      //println("" + m.charAt(x)+ "=" + m.charAt(x).toInt + " => " + m.charAt(x).toInt+key + "=" + (m.charAt(x).toInt+key%('a'.toInt)).asInstanceOf[Char] )
+       if(m.charAt(x) == msg.charAt(x).toUpper){
+         var t:Char = map(msg.charAt(x).toLower)
+         ret += t.toUpper
+       }else{
+         //println(m.charAt(x));
+         ret += map.get(msg.charAt(x).toLower).get
+       }
+       
+      //map += (m.charAt(x), (b.charAt(x).toInt - base)
+    }
+    //println( "Done")
+    if(blocks !=0){
+      var i = blocks
+      while(i < ret.length ){
+        val (p1, p2) = ret.splitAt(i)
+        ret = p1 + " " + p2
+        i += blocks+1
+      }
+    }
+    
     return ret
   }
   
--- src/util/General.scala
+++ src/util/General.scala
@@ -6,14 +6,66 @@ import cryptographyTransposition.ColumnarTransposition
 //import cryptanalysis._
 import cryptanalysisSubstitution.CaesarCipherSolver
 import cryptanalysisTransposition.ColumnarTranspositionSolver
+import scala.io.StdIn.{readInt,readBoolean}
 
 object General {
   def main(args: Array[String]): Unit = {
-    
+    var str = false
     var cc = new CaesarCipher()
     var ccs = new CaesarCipherSolver()
-    var s = cc.encrypt(1,"abcdefghijklmnopqrstuvwxya",5,"a")
-    var sp = cc.decrypt(1,s,true)
+    var i:Integer = -1
+    var k:String = ""
+    println("e : Encrypt")
+    println("d : Decrypt")
+    println("s : Solve with human assistance")
+    var s = readLine("")
+    //val n = readInt()
+    while(s != ""){
+      println()
+      if(s == "e"){
+        test()
+      }else if(s == "e"){
+        k = readLine("Key: ")
+        try{
+          i = k.toInt
+         } catch {
+           case e: Exception => {
+             str = true
+             s = readLine("Message: ")
+             s = cc.encrypt(k,s)
+           }
+         }
+         if(!str){
+           s = readLine("Message:")
+           s = cc.encrypt(i,s) 
+         }
+        println("E(m) = " + s)
+      }else if(s == "d"){
+        k = readLine("Key: ")
+        s = readLine("Message: ")
+          var sp = cc.decrypt(1,s)
+          println("D(m) = " + sp)
+      }else if(s == "s"){
+          s = readLine("Message: ")
+          ccs.solveBruteForceInt(s)
+      }
+      println()
+      // reset
+      str = false
+      println("e : Encrypt")
+      println("d : Decrypt")
+      println("s : Solve with human assistance")
+      s = readLine("")
+    }
+    
+  }
+  
+  
+  def test(){
+    var cc = new CaesarCipher()
+    var ccs = new CaesarCipherSolver()
+    var s = cc.encrypt(1,"abcdefghijklmnopqrstuvwxyz")
+    var sp = cc.decrypt(1,s)
     //ccs.solveBruteForceInt(s)
     println(s )
     println(sp)
@@ -29,4 +81,5 @@ object General {
     s = ctcs.displayColumns(5,s)
     //println(s)
   }
+  
 }
